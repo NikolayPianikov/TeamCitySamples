ebefe11700804c490fdc5438a16a87ebcb341671
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2019-05-24 19:21:43 +0300

    add notnet core mstest

---
+++ dotnet-core-tests-mstests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnet_core_tests_mstests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
